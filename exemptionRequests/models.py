5d7635bfc9ba6b10f3379ae6bacdd5a883d38e40
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-09-01 16:56:13 -0300

    refactor: Alterando tipagem do cpf de number para string

--- exemptionRequests/models.py
+++ exemptionRequests/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.core.validators import MinLengthValidator
 from uuid import uuid4
 
 # Create your models here.
@@ -15,10 +16,10 @@ class ExemptionRequests(models.Model):
     LIGHTNING = 'RAIO', _('RAIO')
   
   id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-  name = models.CharField(max_length=255)
+  name = models.CharField(max_length=255, validators=[MinLengthValidator(10)])
   email = models.EmailField(max_length=255)
   date = models.DateField()
-  cpf = models.IntegerField()
+  cpf = models.CharField(max_length=11, validators=[MinLengthValidator(11)])
   latitude_location = models.FloatField()
   longitude_location = models.FloatField()
   event = models.CharField(max_length=15, choices=EventList.choices)

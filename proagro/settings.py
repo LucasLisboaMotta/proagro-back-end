5ce56a92902f1e0417507665342eb7454e69e3f4
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-09-02 19:40:59 -0300

    fix: problema com o cors corrigido

--- proagro/settings.py
+++ proagro/settings.py
@@ -135,3 +135,12 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:8000",
 ]
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
